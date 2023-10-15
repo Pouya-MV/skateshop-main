a41f5e9cdb1266fca7ce85636187b9e431b46380
Pouya Mousayi <pouyamousayi0101@gmail.com>
2023-10-15 23:15:23 +0330

    first commit

---
+++ src/app/(lobby)/page.tsx
@@ -0,0 +1,222 @@
+import Link from "next/link"
+import { db } from "@/db"
+import { products, stores } from "@/db/schema"
+import { desc, eq, sql } from "drizzle-orm"
+import { Balancer } from "react-wrap-balancer"
+
+import { productCategories } from "@/config/products"
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge"
+import { buttonVariants } from "@/components/ui/button"
+import { CategoryCard } from "@/components/cards/category-card"
+import { ProductCard } from "@/components/cards/product-card"
+import { StoreCard } from "@/components/cards/store-card"
+import { Icons } from "@/components/icons"
+import { Shell } from "@/components/shells/shell"
+
+export default async function IndexPage() {
+  const someProducts = await db
+    .select({
+      id: products.id,
+      name: products.name,
+      images: products.images,
+      category: products.category,
+      price: products.price,
+      inventory: products.inventory,
+      stripeAccountId: stores.stripeAccountId,
+    })
+    .from(products)
+    .limit(8)
+    .leftJoin(stores, eq(products.storeId, stores.id))
+    .groupBy(products.id)
+    .orderBy(desc(stores.stripeAccountId), desc(products.createdAt))
+
+  const someStores = await db
+    .select({
+      id: stores.id,
+      name: stores.name,
+      description: stores.description,
+      stripeAccountId: stores.stripeAccountId,
+    })
+    .from(stores)
+    .limit(4)
+    .leftJoin(products, eq(products.storeId, stores.id))
+    .groupBy(stores.id)
+    .orderBy(desc(stores.stripeAccountId), desc(sql<number>`count(*)`))
+
+  async function getGithubStars(): Promise<number | null> {
+    try {
+      const response = await fetch(
+        "https://api.github.com/repos/sadmann7/skateshop",
+        {
+          headers: {
+            Accept: "application/vnd.github+json",
+          },
+          next: {
+            revalidate: 60,
+          },
+        }
+      )
+
+      if (!response.ok) {
+        return null
+      }
+
+      const data = (await response.json()) as { stargazers_count: number }
+
+      return data.stargazers_count
+    } catch (err) {
+      console.error(err)
+      return null
+    }
+  }
+
+  const githubStars = await getGithubStars()
+
+  return (
+    <Shell className="gap-12">
+      <section
+        id="hero"
+        aria-labelledby="hero-heading"
+        className="mx-auto flex w-full max-w-[64rem] flex-col items-center justify-center gap-4 pb-8 pt-6 text-center md:pb-12 md:pt-10 lg:py-28"
+      >
+        {githubStars ? (
+          <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+            <Badge
+              aria-hidden="true"
+              className="rounded-md px-3.5 py-1.5"
+              variant="secondary"
+            >
+              <Icons.gitHub className="mr-2 h-3.5 w-3.5" />
+              {githubStars} stars on GitHub
+            </Badge>
+            <span className="sr-only">GitHub</span>
+          </Link>
+        ) : null}
+        <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl lg:leading-[1.1]">
+          An e-commerce skateshop built with everything new in Next.js 13
+        </h1>
+        <Balancer className="max-w-[46rem] text-lg text-muted-foreground sm:text-xl">
+          Buy and sell skateboarding products from independent brands and stores
+          around the world
+        </Balancer>
+        <div className="flex flex-wrap items-center justify-center gap-4">
+          <Link href="/products" className={cn(buttonVariants())}>
+            Buy now
+            <span className="sr-only">Buy now</span>
+          </Link>
+          <Link
+            href="/dashboard/stores"
+            className={cn(
+              buttonVariants({
+                variant: "outline",
+              })
+            )}
+          >
+            Sell now
+            <span className="sr-only">Sell now</span>
+          </Link>
+        </div>
+      </section>
+      <section
+        id="categories"
+        aria-labelledby="categories-heading"
+        className="space-y-6 py-6 md:pt-10 lg:pt-24"
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+          <h2 className="text-3xl font-bold leading-[1.1] sm:text-3xl md:text-5xl">
+            Categories
+          </h2>
+          <Balancer className="max-w-[46rem] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+            Find the best skateboarding gears from stores around the world
+          </Balancer>
+        </div>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {productCategories.map((category) => (
+            <CategoryCard key={category.title} category={category} />
+          ))}
+        </div>
+      </section>
+      <section
+        id="featured-products"
+        aria-labelledby="featured-products-heading"
+        className="space-y-6"
+      >
+        <div className="flex items-center">
+          <h2 className="flex-1 text-2xl font-medium sm:text-3xl">
+            Featured products
+          </h2>
+          <Link
+            href="/products"
+            className={cn(
+              buttonVariants({
+                size: "sm",
+              })
+            )}
+          >
+            View all
+            <span className="sr-only">View all products</span>
+          </Link>
+        </div>
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {someProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+      <section
+        id="featured-stores"
+        aria-labelledby="featured-stores-heading"
+        className="space-y-6"
+      >
+        <div className="flex items-center">
+          <h2 className="flex-1 text-2xl font-medium sm:text-3xl">
+            Featured stores
+          </h2>
+          <Link
+            href="/stores"
+            className={cn(
+              buttonVariants({
+                size: "sm",
+              })
+            )}
+          >
+            View all
+            <span className="sr-only">View all stores</span>
+          </Link>
+        </div>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {someStores.map((store) => (
+            <StoreCard
+              key={store.id}
+              store={store}
+              href={`/products?store_ids=${store.id}`}
+            />
+          ))}
+        </div>
+      </section>
+      <section
+        id="random-subcategories"
+        aria-labelledby="random-subcategories-heading"
+        className="flex flex-wrap items-center justify-center gap-4 pb-4"
+      >
+        {productCategories[
+          Math.floor(Math.random() * productCategories.length)
+        ]?.subcategories.map((subcategory) => (
+          <Link
+            key={subcategory.slug}
+            href={`/categories/${String(productCategories[0]?.title)}/${
+              subcategory.slug
+            }`}
+          >
+            <Badge variant="secondary" className="rounded px-3 py-1">
+              {subcategory.title}
+            </Badge>
+            <span className="sr-only">{subcategory.title}</span>
+          </Link>
+        ))}
+      </section>
+    </Shell>
+  )
+}
